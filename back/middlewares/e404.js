bd6889a5fc3c431c713435b870c39d82d4e60603
vinjadevix <vinjadevix@gmail.com>
2021-01-06 00:57:48 +0100

    update details message

--- back/middlewares/e404.js
+++ back/middlewares/e404.js
@@ -9,8 +9,10 @@
  */
 function e404(req, res, next) {
   console.log('entra!');
-  const err = new Error('Not Found');
+  const err = new Error();
+  err.ok = false;
   err.code = 404;
+  err.details = `${req.originalUrl} Not found`;
   next(err);
 }
 module.exports = { e404 };
