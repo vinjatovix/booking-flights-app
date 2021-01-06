d81078b48fc4734a6c9d8ff2b710a20258003181
MattCo23 <mateo_2992@hotmail.com>
2021-01-06 23:07:11 +0100

    Acceso a los datos a mostrar y selección de los mismos

--- back/controllers/booking/userBookings.js
+++ back/controllers/booking/userBookings.js
@@ -2,7 +2,12 @@
 
 const path = require('path');
 const jwt = require('jsonwebtoken');
-const { getBookings } = require('../../repositories/booking/booking-repository');
+const {
+  getBookings,
+  getBookingDetail,
+  getFligthData,
+  getAirport,
+} = require('../../repositories/booking/booking-repository');
 
 /**
  *  Enters a string a returns a formatted booking object key
@@ -17,15 +22,39 @@ async function userBookings(req, res, next) {
     const token = req.headers.authorization;
     const decoded = jwt.decode(token);
     // Hacemos una consulta a la DB y recuperamos los datos de su reserva
-    const result = await getBookings(decoded.id);
-    console.log(result);
-
-    if (result.length === 0) {
+    const RC_bookings = await getBookings(decoded.id);
+    // Comprobamos que el usuario tenga alguna reserva hecha
+    if (RC_bookings.length === 0) {
       const error = new Error();
       error.code = 400;
       error.details = 'User doesnt have active bookings';
       throw error;
     }
+    console.log(RC_bookings);
+    // Recuperamos los datos de la Reserva en detalle
+    const RD_bookings = await getBookingDetail(RC_bookings[0].RC_ID);
+    console.log(RD_bookings);
+    // Recuperamos los datos de los vuelos asignados a esa reserva
+    const fligthData = await getFligthData([RD_bookings[0].RD_VueID, RD_bookings[1].RD_VueID]);
+    console.log(fligthData);
+    //Recuperamos el nombre de los aeropuertos
+    const v1_airport = await getAirport(fligthData[0].Vue_origenID);
+    const v2_airport = await getAirport(fligthData[0].Vue_destinoID);
+    console.log(v1_airport);
+
+    const data = [
+      {
+        vuelo_Ida: {
+          origen: v1_airport.Aero_nombre,
+          destino: v2_airport.Aero_nombre,
+          salida: fligthData[0].Vue_horaSalida,
+          llegada: fligthData[0].Vue_horaLlegada,
+          plazas: RD_bookings[0].RD_adults,
+          escalas: fligthData[0].Vue_paradas,
+          estado: RC_bookings[0].RC_status,
+        },
+      },
+    ];
 
     res.send('Aquí acaba');
   } catch (error) {
--- back/repositories/booking/booking-repository.js
+++ back/repositories/booking/booking-repository.js
@@ -3,6 +3,14 @@
 const { createBookingDetail } = require('./createBookingDetail');
 const { createBookingHeader } = require('./createBookingHeader');
 const { createFlight } = require('./createFlight');
-const { getBookings } = require('./getUserBookings');
+const { getBookings, getBookingDetail, getFligthData, getAirport } = require('./showBookings');
 
-module.exports = { createFlight, createBookingHeader, createBookingDetail, getBookings };
+module.exports = {
+  createFlight,
+  createBookingHeader,
+  createBookingDetail,
+  getBookings,
+  getBookingDetail,
+  getFligthData,
+  getAirport,
+};
