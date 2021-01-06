00a7a17fcbd8428a408984f350378cc2fe5c98b4
vinjadevix <vinjadevix@gmail.com>
2021-01-06 03:56:59 +0100

    refactoring post login

---
+++ back/controllers/user/userStatusManager.js
@@ -0,0 +1,25 @@
+'use strict';
+const userRepository = require('../../repositories/user-repository');
+const path = require('path');
+
+async function userStatusManager(user) {
+  if (!['a', 'i', 'e'].includes(user.Usr_status)) {
+    const error = new Error();
+    error.ok = false;
+    error.code = 418;
+    error.details = 'Who are you?';
+    error.file = path.basename(__filename);
+    throw error;
+  }
+  if (user.Usr_status === 'i') {
+    await userRepository.changeStatus(['a', user.Usr_ID]);
+  } else if (user.Usr_status === 'e') {
+    const error = new Error();
+    error.ok = false;
+    error.code = 403;
+    error.details = 'Invalid credentials, this account has been deleted';
+    throw error;
+  }
+  return true;
+}
+exports.userStatusManager = userStatusManager;
