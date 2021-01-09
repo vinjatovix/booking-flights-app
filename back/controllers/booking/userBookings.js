5d14619ef38f2dfe7630cd1ade64f19b42437d0f
MattCo23 <mateo_2992@hotmail.com>
2021-01-09 19:15:14 +0100

    Modificada la estructura de los datos mostrados en la solicitud de reservas de un usuario

--- back/controllers/booking/userBookings.js
+++ back/controllers/booking/userBookings.js
@@ -31,19 +31,19 @@ async function userBookings(req, res, next) {
       error.details = 'User doesnt have active bookings';
       throw error;
     }
-    console.log('Reserva Cabecera', RC_bookings);
     // Recuperamos los datos de la Reserva en detalle
     const RD_bookings = await getBookingDetail(RC_bookings[0].RC_ID);
-    console.log('Reserva Detalle', RD_bookings);
     // Recuperamos los datos de los vuelos asignados a esa reserva
     const fligthData = await getFligthData([RD_bookings[0].RD_VueID, RD_bookings[1].RD_VueID]);
-    console.log('Datos de Vuelos', fligthData);
     //Recuperamos el nombre de los aeropuertos
     const v1_airport1 = await getAirport(fligthData[0].Vue_origenID);
     const v1_airport2 = await getAirport(fligthData[0].Vue_destinoID);
+    const v2_airport1 = await getAirport(fligthData[1].Vue_origenID);
+    const v2_airport2 = await getAirport(fligthData[1].Vue_destinoID);
     //Recuperamos el nombre de las compañías
     const v1_company = await getCompany(fligthData[0].Vue_companyID);
-    console.log(v1_airport1);
+    const v2_company = await getCompany(fligthData[1].Vue_companyID);
+
     const data = [
       {
         vuelo_Ida: {
@@ -53,32 +53,29 @@ async function userBookings(req, res, next) {
           llegada: fligthData[0].Vue_horaLlegada,
           plazas: RD_bookings[0].RD_adults,
           escalas: fligthData[0].Vue_paradas,
-          estado: RC_bookings[0].RC_status,
           compañía: v1_company[0].Cmp_nombre,
           duración: fligthData[0].Vue_duracion,
         },
-        // vuelo_Vuelta: {
-        //   origen: v1_airport2[1].Aero_nombre,
-        //   destino: v1_airport1[1].Aero_nombre,
-        //   salida: fligthData[1].Vue_horaSalida,
-        //   llegada: fligthData[1].Vue_horaLlegada,
-        //   plazas: RD_bookings[1].RD_adults,
-        //   escalas: fligthData[1].Vue_paradas,
-        //   estado: RC_bookings[1].RC_status,
-        //   compañía: v1_company[1].Cmp_nombre,
-        //   duración: fligthData[1].Vue_duracion,
-        // },
-        // precio: {
-        //   por_persona: RC_bookings[0].RC_base,
-        //   total: RC_bookings[0].RC_total,
-        // },
+        vuelo_Vuelta: {
+          origen: v2_airport1[0].Aero_nombre,
+          destino: v2_airport2[0].Aero_nombre,
+          salida: fligthData[1].Vue_horaSalida,
+          llegada: fligthData[1].Vue_horaLlegada,
+          plazas: RD_bookings[1].RD_adults,
+          escalas: fligthData[1].Vue_paradas,
+          compañía: v2_company[0].Cmp_nombre,
+          duración: fligthData[1].Vue_duracion,
+        },
+        detalles: {
+          por_persona: RC_bookings[0].RC_base,
+          total: RC_bookings[0].RC_total,
+          estado: RC_bookings[0].RC_status,
+        },
       },
     ];
 
     res.send(data);
   } catch (error) {
-    // TODO: Si algo falla que borre todo lo escrito en la base de esta operacion
-
     if (error.name === 'ValidationError') {
       error.code = 400;
       error.file = path.basename(__filename);
