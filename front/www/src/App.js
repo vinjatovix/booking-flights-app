81ff7af0ad9aa6324be9bd7b522bcd85039ee8f3
MattCo23 <mateo_2992@hotmail.com>
2021-01-28 19:29:17 +0100

    Inicializado React Router y definidas primeras rutas

--- front/www/src/App.js
+++ front/www/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Redirect,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <h1>Inicio</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/register">
+            <h1>Register</h1>
+          </Route>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
