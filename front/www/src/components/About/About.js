d1592ae7bdc2a15ddfd34206dde3edd66b97f42f
vinjadevix <vinjadevix@gmail.com>
2021-01-30 03:09:06 +0100

    About cmp cleaned and updated with initial load

--- front/www/src/components/About/About.js
+++ front/www/src/components/About/About.js
@@ -1,35 +1,21 @@
-import React from "react";
-import "./about.css";
-import { Article } from "../common/Article";
-import { ListDrawer } from "../common/ListDrawer/ListDrawer";
-import { ArticleWrapper } from "../common/ArticleWrapper";
-import { useRemoteUrl } from "../../hooks/useRemoteUrl";
+import React from 'react';
+import './about.css';
+import { Article } from '../common/Article';
+import { ListDrawer } from '../common/ListDrawer/ListDrawer';
+import { ArticleWrapper } from '../common/ArticleWrapper';
+import { useRemoteUrl } from '../../hooks/useRemoteUrl';
 // import { useRemoteUrl } from "../../hooks/useRemoteUrl";
 
 export const About = () => {
-  const [aboutInfo, setAboutInfo] = useRemoteUrl("http://localhost:8337/about");
-  const {
-    logo,
-    info,
-    formers,
-    stack,
-    tools,
-    links,
-    credits,
-    thanks,
-  } = aboutInfo;
+  const [aboutInfo] = useRemoteUrl('http://localhost:8337/about');
+  const { logo, info, formers, stack, tools, links, credits, thanks } = aboutInfo;
   const res = (
     <>
       <ArticleWrapper>
         <Article className="about" title="About">
           <img className="about-logo" src={logo} alt="logo" />
           <ListDrawer type="p" title="Info" items={info}></ListDrawer>
-          <ListDrawer
-            type="links"
-            title="Enlaces"
-            cssClassName="aboutLinks"
-            items={links}
-          />
+          <ListDrawer type="links" title="Enlaces" cssClassName="aboutLinks" items={links} />
         </Article>
       </ArticleWrapper>
 
@@ -48,8 +34,10 @@ export const About = () => {
       </ArticleWrapper>
     </>
   );
-  return <>
-  {!aboutInfo?.ok && "loading...."}
-  {aboutInfo.ok && res}
-  </>;
+  return (
+    <>
+      {!aboutInfo?.ok && 'loading....'}
+      {aboutInfo.ok && res}
+    </>
+  );
 };
