8dc5b0a52bf2f3a493a12dce9437ae80c3a2c0ee
vinjadevix <vinjadevix@gmail.com>
2021-01-30 06:44:45 +0100

    uuid on paragraphs

--- front/www/src/components/common/ListDrawer/ParagraphList.js
+++ front/www/src/components/common/ListDrawer/ParagraphList.js
@@ -1,12 +1,12 @@
-import React from "react";
-
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export const ParagraphList = (props) => {
-    return (
-        <>
-            {props.items.map((p) => {
-                return <p key={`p${p.id}`}>{p.text}</p>;
-            })}
-        </>
-    );
+  return (
+    <>
+      {props.items.map((p) => {
+        return <p key={uuidv4()}>{p.text}</p>;
+      })}
+    </>
+  );
 };
