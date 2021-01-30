161a91e7dd30fc4b595aac8b45b3b9f0bb1502d8
vinjadevix <vinjadevix@gmail.com>
2021-01-30 01:11:22 +0100

    uuid intalled and applied to footer

--- front/www/src/components/common/ListDrawer/LinkList.js
+++ front/www/src/components/common/ListDrawer/LinkList.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { ListItem } from "./ListItem";
-
+import { v4 as uuidv4 } from "uuid";
 
 export const LinkList = (props) => {
-    return (
-        <>
-            <h3>{props.title}</h3>
-            <ul className={props.cssClassName}>
-                {props.items.map((item) => {
-                    return (
-                        <ListItem key={`a${item.name}`} name={item.name}>
-                            <a href={item.url} children={item.linkText || item.children} />
-                        </ListItem>
-                    );
-                })}
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <h3>{props.title}</h3>
+      <ul className={props.cssClassName}>
+        {props.items.map((item) => {
+          return (
+            <ListItem key={`a${uuidv4()}}`} name={item.name}>
+              <a href={item.url} children={item.linkText || item.children} />
+            </ListItem>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
