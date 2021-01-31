00449e0a708086388b46ca6ef221af993a22f787
vinjadevix <vinjadevix@gmail.com>
2021-01-31 03:30:29 +0100

    joi error response formating to match front needs

--- back/config/loggers/winston.js
+++ back/config/loggers/winston.js
@@ -60,6 +60,17 @@ function winstonCatch() {
   return function (err, req, res, next) {
     if (err.name === 'ValidationError') {
       err.code = 400;
+      err.details = err.details[0].message;
+      console.log('WINSTON', err.details);
+      err.details = err.details.replace(
+        '"password" length must be at least 6 characters long',
+        'Longitud mínima de contraseña 6'
+      );
+      err.details = err.details.replace('"repeatPassword" must be [ref:password]', 'Las contraseñas no coinciden');
+      err.details = err.details.replace(
+        '"username" length must be at least 5 characters long',
+        'Longitud mínima de usuario 5'
+      );
     }
     if (err.code === 'EAI_AGAIN') {
       err.message = `${process.env.DATABASE_HOST} is'n a known host.`;
@@ -79,7 +90,7 @@ function winstonCatch() {
     }
     err.ok = err.ok || false;
     err.code = err.code || 500;
-    err.details = err.details || 'Unknown error...'
+    err.details = err.details || 'Unknown error...';
     logThis(err, req);
     res.status(err.code).json(err);
   };
