2a09beda85f45bbf74b18a6fa0c205ff80f0d521
vinjadevix <vinjadevix@gmail.com>
2021-01-31 05:01:43 +0100

    added footer aux tools while dev

--- front/www/src/components/common/Footer.js
+++ front/www/src/components/common/Footer.js
@@ -1,18 +1,29 @@
-import React from "react";
-import { ListDrawer } from "./ListDrawer/ListDrawer";
+import React, { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+import { ListDrawer } from './ListDrawer/ListDrawer';
 
 export const Footer = () => {
+  const [, setAuth] = useContext(AuthContext);
   return (
     <footer className="app-footer">
       Code-Vix &copy; 2021 FLanders v0.6
       <ListDrawer
         type="links"
         items={[
-          { children: "login", url: "/login" },
-          { children: "register", url: "/register" },
-          { children: "about", url: "/about" },
+          { children: 'login', url: '/login' },
+          { children: 'register', url: '/register' },
+          { children: 'about', url: '/about' },
+          { children: 'search', url: '/' },
         ]}
       />
+      <div
+        onClick={(e) => {
+          e.preventDefault();
+          setAuth('');
+        }}
+      >
+        x
+      </div>
     </footer>
   );
 };
