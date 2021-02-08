6e1a6a364741a9dfae34b0f54a7c542a31c865ce
vinjadevix <vinjadevix@gmail.com>
2021-02-08 05:55:52 +0100

    feat(searchFlight): :sparkles: added passenger counter custom hook
    now we can increment or decrement passengers by pressing buttons

--- front/www/src/components/SearchFlight/PassengerCounter.jsx
+++ front/www/src/components/SearchFlight/PassengerCounter.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as A from '../../context/flight/Flight.actions';
+import { useCounter } from '../../hooks/useCounter';
 
 export const PassengerCounter = ({ adults, dispatch }) => {
+  const { state: passengers, increment, decrement } = useCounter(adults);
+
+  useEffect(() => {
+    dispatch(A.setNumber({ name: 'adults', value: passengers }));
+  }, [passengers, dispatch]);
+
   return (
     <fieldset className="SearchForm__passengers">
-      <input className="SearchForm__passengers-trigger" type="button" value="-" />
+      <input
+        className="SearchForm__passengers-trigger"
+        type="button"
+        value="-"
+        onClick={() => {
+          decrement();
+        }}
+      />
       <input
         className="SearchForm__passengers-value "
         type="number"
         name="passengers"
         id="passengers"
         placeholder="1"
-        onChange={(e) => dispatch(A.setNumber({ name: 'adults', value: e.target.value }))}
-        value={adults} />
-      <input className="SearchForm__passengers-trigger" type="button" value="+" />
+        onChange={(e) => dispatch(A.setNumber({ name: 'adults', value: passengers }))}
+        value={passengers}
+      />
+      <input
+        className="SearchForm__passengers-trigger"
+        type="button"
+        value="+"
+        onClick={() => {
+          increment();
+        }}
+      />
     </fieldset>
   );
 };
