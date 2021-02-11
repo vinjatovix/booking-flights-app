a8b9dbcaae6ae27b6dd1143be9635304a22fdc51
MattCo23 <mateo_2992@hotmail.com>
2021-02-11 19:21:39 +0100

    FEAT: Finish first CSS of profile-data

--- front/www/src/components/Profile/Subsections/ProfileData.jsx
+++ front/www/src/components/Profile/Subsections/ProfileData.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../profile.css';
 import ProfilePhoto from '../../../assets/svg/imagen-de-usuario-con-fondo-negro.svg';
 import leftArrow from '../../../assets/svg/angle-left-solid.svg';
+import edit from '../../../assets/svg/pen-solid.svg';
 import { switchBoolean } from '../../../context/Auth.actions';
 
 export const ProfileData = ({ profile_data, photo, dispatch }) => {
@@ -23,7 +24,30 @@ export const ProfileData = ({ profile_data, photo, dispatch }) => {
           }}
         />
         <img src={logo} alt="foto-de-usuario" />
+        <div className="photo-button"></div>
       </section>
+      <ul className="profile-inputs">
+        <li className="profile-nickname">
+          <div>
+            <h5>Nickname</h5>
+            <h4>MattCo</h4>
+          </div>
+          <img src={edit} alt="boton-de-editar" />
+        </li>
+        <li className="profile-bio">
+          <div>
+            <h5>Biografía</h5>
+            <h4>Hola que tal chavales, como va la cosa, aquí estamos todos a tope de jazz</h4>
+          </div>
+          <img src={edit} alt="boton-de-editar" />
+        </li>
+        <li className="profile-email">
+          <div>
+            <h5>Email</h5>
+            <h4>mateo_2992@hotmail.com</h4>
+          </div>
+        </li>
+      </ul>
     </>
   );
 };
