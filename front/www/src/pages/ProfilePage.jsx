aa2290e327cf2721b4d29ad6324202d5f8c2874c
MattCo23 <mateo_2992@hotmail.com>
2021-02-11 14:22:30 +0100

    FEAT: Created jsx files of subsections from profile

--- front/www/src/pages/ProfilePage.jsx
+++ front/www/src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Profile } from '../components/Profile/Profile';
-import { ProfileData } from '../components/Profile/ProfileData';
+import { ProfileData } from '../components/Profile/Subsections/ProfileData';
 
 export const ProfilePage = ({ control, profile }) => {
   const { profile_data, profile_pass, profile_bookings, profile_tools } = profile;
