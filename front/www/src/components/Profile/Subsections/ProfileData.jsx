9ae6a23f92442448c23a4a69566eeddee0dcabfb
MattCo23 <mateo_2992@hotmail.com>
2021-02-11 16:48:05 +0100

    FEAT: Created button back in subsections

--- front/www/src/components/Profile/Subsections/ProfileData.jsx
+++ front/www/src/components/Profile/Subsections/ProfileData.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import '../profile.css';
+import ProfilePhoto from '../../../assets/svg/imagen-de-usuario-con-fondo-negro.svg';
+import leftArrow from '../../../assets/svg/angle-left-solid.svg';
+import { switchBoolean } from '../../../context/Auth.actions';
 
-export const ProfileData = () => {
-  return <h1>Hola</h1>;
+export const ProfileData = ({ profile_data, photo, dispatch }) => {
+  console.log(profile_data);
+  console.log(photo);
+
+  let logo = '';
+  if (photo === '') {
+    logo = ProfilePhoto;
+  } else logo = photo;
+  return (
+    <>
+      <section className="profile-data-main">
+        <img
+          src={leftArrow}
+          alt="boton-atras"
+          onClick={() => {
+            dispatch(switchBoolean({ name: 'profile_data', value: profile_data }));
+          }}
+        />
+        <img src={logo} alt="foto-de-usuario" />
+      </section>
+    </>
+  );
 };
--- front/www/src/pages/ProfilePage.jsx
+++ front/www/src/pages/ProfilePage.jsx
@@ -3,7 +3,9 @@ import { Profile } from '../components/Profile/Profile';
 import { ProfileData } from '../components/Profile/Subsections/ProfileData';
 
 export const ProfilePage = ({ control, profile }) => {
-  const { profile_data, profile_pass, profile_bookings, profile_tools } = profile;
+  const { profile_data } = profile;
   console.log(profile_data);
-  return <>{profile_data === true ? <ProfileData /> : <Profile {...control} {...profile} />}</>;
+  return (
+    <>{profile_data === true ? <ProfileData {...profile} {...control} /> : <Profile {...control} {...profile} />}</>
+  );
 };
