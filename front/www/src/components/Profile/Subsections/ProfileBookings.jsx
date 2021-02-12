df6576aaa518c4c10961ebf7a05fbf7b7141203d
MattCo23 <mateo_2992@hotmail.com>
2021-02-12 22:13:56 +0100

    FEAT: Created new structure of subsection profile_bookings

--- front/www/src/components/Profile/Subsections/ProfileBookings.jsx
+++ front/www/src/components/Profile/Subsections/ProfileBookings.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import '../profile.css';
 
 export const ProfileBookings = ({ dispatch, profile_bookings }) => {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {}, [bookings]);
   return (
     <>
-      <h1>Hola</h1>
+      <h4>Mis reservas</h4>
     </>
   );
 };
+
+// //       <ul>
+//         {props.items.map((item) => {
+//           return <li>{item}</li>;
+//         })}
+//       </ul>
