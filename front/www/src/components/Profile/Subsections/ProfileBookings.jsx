c58a57bb1544fd58bb8419d9368190b37a308c28
MattCo23 <mateo_2992@hotmail.com>
2021-02-13 19:31:34 +0100

    FEAT: Added useEffect for calling /myBookings

--- front/www/src/components/Profile/Subsections/ProfileBookings.jsx
+++ front/www/src/components/Profile/Subsections/ProfileBookings.jsx
@@ -4,11 +4,28 @@ import '../profile.css';
 
 export const ProfileBookings = ({ dispatch, profile_bookings }) => {
   const [bookings, setBookings] = useState([]);
+  const token = JSON.parse(localStorage.getItem('token'));
 
-  useEffect(() => {}, [bookings]);
+  useEffect(() => {
+    async function getBookings() {
+      const res = await fetch('http://localhost:8337/myBookings', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token,
+        },
+      });
+      const json = await res.json();
+      console.log(json);
+    }
+    getBookings();
+  }, [bookings]);
   return (
     <>
       <h4>Mis reservas</h4>
+      <ul className="flight-offer">
+        <li></li>
+      </ul>
     </>
   );
 };
