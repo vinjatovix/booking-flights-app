186b7b5333c15c1de0e9708448708ca38a4bac2f
MattCo23 <mateo_2992@hotmail.com>
2021-02-13 22:06:36 +0100

    FEAT: Designed CSS of any item of bookings

--- front/www/src/components/Profile/Subsections/ProfileBookings.jsx
+++ front/www/src/components/Profile/Subsections/ProfileBookings.jsx
@@ -16,22 +16,42 @@ export const ProfileBookings = ({ dispatch, profile_bookings }) => {
         },
       });
       const json = await res.json();
-      console.log(json);
+      setBookings(json);
+      console.log(bookings);
     }
     getBookings();
-  }, [bookings]);
+  }, []);
   return (
     <>
       <h4>Mis reservas</h4>
       <ul className="flight-offer">
-        <li></li>
+        {bookings.map((item) => {
+          return (
+            <li>
+              <section className="booking-date">
+                <p>20</p>
+                <p>ENE</p>
+                <p>2020</p>
+              </section>
+              <section className="booking-info">
+                <p className="booking-iata">MAD</p>
+                <div>
+                  <p className="booking-id">ID:234563</p>
+                  <p className="booking-price">108,86€</p>
+                </div>
+                <p className="booking-iata">SCQ</p>
+              </section>
+              {bookings.length > 2 ? (
+                <section className="booking-date">
+                  <p>6</p>
+                  <p>AGO</p>
+                  <p>2020</p>
+                </section>
+              ) : null}
+            </li>
+          );
+        })}
       </ul>
     </>
   );
 };
-
-// //       <ul>
-//         {props.items.map((item) => {
-//           return <li>{item}</li>;
-//         })}
-//       </ul>
