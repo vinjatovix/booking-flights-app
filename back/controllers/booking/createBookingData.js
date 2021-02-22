2a842df801c9a86a04d93e485718ea178395cb16
MattCo23 <mateo_2992@hotmail.com>
2021-02-22 23:24:42 +0100

    FIX: Fixed problem with myBookings data

--- back/controllers/booking/createBookingData.js
+++ back/controllers/booking/createBookingData.js
@@ -14,11 +14,20 @@ async function createBookingData(bookings) {
   for (const booking of bookings) {
     // Recuperamos los datos de las Reservas en detalle
     const RD_bookings = await getBookingDetail(booking.RC_ID);
+    console.log(RD_bookings);
+    // Calculamos el número de escalas de la ida y la vuelta
+    const counter = { ida: 0, vuelta: 0 };
+    for (const booking of RD_bookings) {
+      if (booking.RD_direccion === 0) {
+        counter.ida++;
+      } else counter.vuelta++;
+    }
     // Recuperamos los datos de los vuelos asignados a esa reserva
     const fligthData = await getFligthData(RD_bookings[0].RD_VueID);
+    const fligthData2 = await getFligthData(RD_bookings[RD_bookings.length - 1].RD_VueID);
     //Recuperamos el nombre de los aeropuertos
     const airport1 = await getAirport(fligthData[0].Vue_origenID);
-    const airport2 = await getAirport(fligthData[0].Vue_destinoID);
+    const airport2 = await getAirport(fligthData2[0].Vue_origenID);
     //Recuperamos el nombre de las compañías
     const company = await getCompany(fligthData[0].Vue_companyID);
     const originTown = await getTown(airport1[0].Aero_LocaID);
@@ -31,17 +40,21 @@ async function createBookingData(bookings) {
         salida: fligthData[0].Vue_horaSalida,
         locaOrigen: originTown[0].Loca_nombre,
         locaDesti: destinationTown[0].Loca_nombre,
-        llegada: fligthData[0].Vue_horaLlegada,
-        escalas: fligthData[0].Vue_paradas,
-        compañia: company[0].Cmp_nombre,
+        llegada: fligthData2[0].Vue_horaSalida,
+        escalas: counter.ida - 1,
+        compania: company[0].Cmp_nombre,
         duracion: fligthData[0].Vue_duracion,
       },
     };
 
     if (RD_bookings[RD_bookings.length - 1].RD_direccion === 1) {
-      const fligthData = await getFligthData(RD_bookings[RD_bookings.length - 1].RD_VueID);
+      const originElement = (element) => element.RD_direccion === 1;
+      const originId = RD_bookings.findIndex(originElement);
+
+      const fligthData = await getFligthData(RD_bookings[originId].RD_VueID);
+      const fligthData2 = await getFligthData(RD_bookings[0].RD_VueID);
       const airport1 = await getAirport(fligthData[0].Vue_origenID);
-      const airport2 = await getAirport(fligthData[0].Vue_destinoID);
+      const airport2 = await getAirport(fligthData2[0].Vue_origenID);
       const company = await getCompany(fligthData[0].Vue_companyID);
       const originTown = await getTown(airport1[0].Aero_LocaID);
       const destinationTown = await getTown(airport2[0].Aero_LocaID);
@@ -53,8 +66,8 @@ async function createBookingData(bookings) {
         locaDesti: destinationTown[0].Loca_nombre,
         salida: fligthData[0].Vue_horaSalida,
         llegada: fligthData[0].Vue_horaLlegada,
-        escalas: fligthData[0].Vue_paradas,
-        compañia: company[0].Cmp_nombre,
+        escalas: counter.vuelta - 1,
+        compania: company[0].Cmp_nombre,
         duracion: fligthData[0].Vue_duracion,
       };
     }
