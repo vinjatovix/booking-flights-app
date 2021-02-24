53d628eeceb04e08ece93eb04b2e6ef7aa212065
vinjadevix <vinjadevix@gmail.com>
2021-02-24 06:25:30 +0100

    fix: :art: disclaimer update

--- front/www/src/components/common/Disclaimer/Disclaimer.jsx
+++ front/www/src/components/common/Disclaimer/Disclaimer.jsx
@@ -9,14 +9,20 @@ export const Disclaimer = () => {
     <section className="Disclaimer__section">
       <h1>Disclaimer</h1>
       <p>
-        Aunque los datos de los vuelos son reales, esta aplicación es una simple demo de conocimientos adquiridos por
-        Mateo Codesido y Pablo Viña durante el bootcamp JSB07CO de Hack A Boss en 2020/2021.
+        Esta aplicación es una demo de conocimientos adquiridos por Mateo Codesido y Pablo Viña durante el bootcamp JSB07CO de Hack A Boss en 2020/2021.
       </p>
-      <p>Esta reserva no tiene valor real ni nos hacemos responsables del uso indebido de la aplicación.</p>
+      <p>Los datos son reales pero esta reserva no tiene valor real ni nos hacemos responsables del uso indebido de la aplicación.</p>
       <hr />
-      <h1>Privacy by Design</h1>
-      <p>De acuerdo con la RGPD, esta aplicación no requiere de datos personales para su uso.</p>
-      <p>Tampoco almacenamos cookies. </p>
+      <h4>Privacy by Design</h4>
+      <p>
+        De acuerdo con el principio de minimización de datos del RGPD, esta aplicación no requiere de datos personales
+        para su uso.
+      </p>{' '}
+      <p>Los datos proporcionados serán mínimos y adecuados a la prestación del servicio (email).</p>
+      <p>
+        El tratamiento de los datos será leal y transparente. Puede eliminar su cuenta en cualquier momento desde el
+        panel de control. Este proceso será irreversible.
+      </p>
       <Input
         type="submit"
         value="Ok! ✈️ GO "
@@ -25,7 +31,6 @@ export const Disclaimer = () => {
         onClick={(e) => {
           e.preventDefault();
           dispatch(switchBoolean({ name: e.target.name, value: false }));
-          console.log('ok');
         }}
       />
     </section>
